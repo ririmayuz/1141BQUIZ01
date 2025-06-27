c3076ad2cda922227709abc98117fe39081d3bd6
Yuz <wwowkdrr@gmail.com>
2025-06-27 09:46:30 +0800

    整合成一個檔案insert.php

--- web14/model/title.php
+++ web14/model/title.php
@@ -1,7 +1,7 @@
 <h3 style='text-align: center;'>新增標題區圖片</h3>
         <hr>
         <!-- 這裡的api是在js拿出來的另一個beckend下面的api -->
-        <form action="./api/insert_title.php" method="post" enctype="multipart/form-data">
+        <form action="./api/insert.php" method="post" enctype="multipart/form-data">
             <div>
                 <label for="">標題區圖片：</label>
                 <input type="file" name="img">
@@ -11,6 +11,7 @@
                 <input type="text" name="text">
             </div>
             <div>
+                <input type="hidden" name="table" value="title">
                 <input type="submit" value="新增">
                 <input type="reset" value="重置">
             </div>
