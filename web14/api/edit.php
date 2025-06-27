a582c23522094a3abcf87e7ea75f5bc73e73aa60
Yuz <wwowkdrr@gmail.com>
2025-06-27 16:31:11 +0800

    完成主選單管理功能

--- web14/api/edit.php
+++ web14/api/edit.php
@@ -22,6 +22,9 @@ foreach ($_POST['id'] as $key => $id) {
                 break;
 
             case "menu":
+                $row['text']=$_POST['text'][$key];
+                $row['href']=$_POST['href'][$key];
+                $row['sh'] = (isset($_POST['sh']) && in_array($id, $_POST['sh'])) ? 1 : 0;
                 break;
 
             default:
