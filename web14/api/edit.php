f3202bb04ba8b556f74cbadb84f6027d51488deb
Yuz <wwowkdrr@gmail.com>
2025-06-27 15:46:13 +0800

    寫好news跟iamge的功能，並修改edit.php,以適應不同的功能欄位

--- web14/api/edit.php
+++ web14/api/edit.php
@@ -1,42 +1,33 @@
-<?php 
+<?php
 include_once "db.php";
 
-$table=$_POST['table'];
-$db=${ucfirst($table)};
+$table = $_POST['table'];
+$db = ${ucfirst($table)};
 
-foreach($_POST['id'] as $key => $id){
-    if(isset($_POST['del']) && in_array($id,$_POST['del'])){
+foreach ($_POST['id'] as $key => $id) {
+    if (isset($_POST['del']) && in_array($id, $_POST['del'])) {
         $db->del($id);
-    }else{
-        $row=$db->find($id);
+    } else {
+        $row = $db->find($id);
         //dd($row);
-        switch($table){
+        switch ($table) {
             case "title":
-                $row['text']=$_POST['text'][$key];
-                $row['sh']=($_POST['sh']==$id)?1:0;
-            break;
-
-            case "ad":
-                $row['text']=$_POST['text'][$key];
-                $row['sh']=(isset($_POST['sh']) && in_array($id,$_POST['sh']))?1:0;
-            break;
-
-            case "mvim":
-                $row['sh']=(isset($_POST['sh']) && in_array($id,$_POST['sh']))?1:0;
-            break;
-
-            case "image":
-            break;
-
-            case "news":
-            break;
+                $row['text'] = $_POST['text'][$key];
+                $row['sh'] = ($_POST['sh'] == $id) ? 1 : 0;
+                break;
 
             case "admin":
-            break;
-            
-            case "menu":
-            break;
+                break;
 
+            case "menu":
+                break;
+
+            default:
+                if (isset($row['text'])) {
+                    $row['text'] = $_POST['text'][$key];
+                }
+                $row['sh'] = (isset($_POST['sh']) && in_array($id, $_POST['sh'])) ? 1 : 0;
+                break;
         }
         $db->save($row);
         //dd($row);
@@ -45,5 +36,3 @@ foreach($_POST['id'] as $key => $id){
 
 
 to("../backend.php?do=$table");
-
-?>
