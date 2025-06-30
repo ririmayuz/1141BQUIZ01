80d158bc9aa73e474b39dc24121f7b282abd53d4
Yuz <wwowkdrr@gmail.com>
2025-06-30 10:01:13 +0800

    完成頁尾版權功能，修改檔案錯字

--- web14/backend/bottom.php
+++ web14/backend/bottom.php
@@ -1,20 +1,20 @@
 <div style="width:99%; height:87%; margin:auto; overflow:auto; border:#666 1px solid;">
-    <p class="t cent botli">進站總人數管理</p>
+    <p class="t cent botli">頁尾版權資料管理</p>
     <form method="post" action="./api/edit_column.php">
-        <table width="50%" style="margin:auto">
+        <table width="50%"  style="margin:auto;">
             <tbody>
                 <tr class="yel">
-                    <td width="50%">進站總人數：</td>
+                    <td width="50%">頁尾版權資料</td>
+                    <?php
+                    $row=$Bottom->find(1);
+                    ?>
                     <td width="50%">
-                        <?php
-                        $row = $Total->find(1);
-
-                        //${ucfirst($do)}->find(1)['total']
-                        ?>
-                        <input type="text" name="total" value="<?= $row['total']; ?>" style="width:90%">
+                        <input type="text" name="bottom" value="<?= $row['bottom'];?>" style="text-align: left; width=90%;">
                         <input type="hidden" name="id" value="<?= $row['id']; ?>">
-                    </td>
+                    </td>  
                 </tr>
+
+
             </tbody>
         </table>
 
@@ -23,8 +23,8 @@
                 <tr>
                     <input type="hidden" name="table" value="<?= $do; ?>">
                     <td width="200px"></td>
-                    <td class="cent"><input type="submit" value="修改確定">
-                    <input type="reset" value="重置"></td>
+                    <td class="cent"><input type="submit" value="修改確定"><input
+                            type="reset" value="重置"></td>
                 </tr>
             </tbody>
         </table>
