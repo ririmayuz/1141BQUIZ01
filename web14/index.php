484945f446f6e267decb09839fbc534dcbdc3822
Yuz <wwowkdrr@gmail.com>
2025-07-01 14:17:26 +0800

    校園映像區完成顯示

--- web14/index.php
+++ web14/index.php
@@ -39,8 +39,8 @@
             </div>
 
             <?php
-            $do=$_GET['do']??'main';
-            $file="./front/".$do."php";
+            $do = $_GET['do'] ?? 'main';
+            $file = "./front/" . $do . "php";
             ?>
 
             <?php
@@ -55,16 +55,32 @@
             }
             ?>
 
-            
+
             <div class="di di ad" style="height:540px; width:23%; padding:0px; margin-left:22px; float:left; ">
                 <!--右邊-->
                 <button style="width:100%; margin-left:auto; margin-right:auto; margin-top:2px; height:50px;"
                     onclick="lo('?do=admin')">管理登入</button>
                 <div style="width:89%; height:480px;" class="dbor">
                     <span class="t botli">校園映象區</span>
+                    <div class='cent' onclick="pp(1)">
+                        <img src="./icon/up.jpg" alt="">
+                    </div>
+                    <?php
+                    $images = $Image->all(['sh' => 1]);
+                    foreach ($images as $key => $image):
+                    ?>
+                        <div class='cent im' id="ssaa<?= $key; ?>">
+                            <img src="./images/<?= $image['img']; ?>" style="width:150px;height:103px;border:3px solid orange;margin:2px;">
+                        </div>
+                    <?php
+                    endforeach;
+                    ?>
+                    <div class='cent' onclick="pp(2)">
+                        <img src="./icon/dn.jpg" alt="">
+                    </div>
                     <script>
                         var nowpage = 0,
-                            num = 0;
+                            num = <?= $Image->count(['sh' => 1]); ?>;
 
                         function pp(x) {
                             var s, t;
