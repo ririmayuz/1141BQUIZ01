3c2cd1f213b9c8a7818f47c1abcbc5ec7377b275
Yuz <wwowkdrr@gmail.com>
2025-07-04 09:27:04 +0800

    管理登入功能完成

--- web14/api/login.php
+++ web14/api/login.php
@@ -1,7 +1,12 @@
 <?php
 include_once "db.php";
-if($_POST['acc']=='admin' && $_POST['pw']=='1234'){
-    to("../backend.php");
+
+// $user=$Admin->find(['acc'=>$_POST['acc'],'pw'=>$_POST['pw']]);
+$user=$Admin->count($_POST);
+
+// if(!empty($user)){
+if($user){
+to("../backend.php");
 }else{
     echo "<script>alert('帳號或密碼錯誤');location.replace('../index.php?do=login')</script>";
 }
